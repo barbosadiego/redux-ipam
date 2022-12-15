d418decdbeef312ac42cb8a574f02e852c64761d
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-15 15:24:38 -0300

    add cidades actions creator

---
+++ src/actionCreators/cidadesActions.js
@@ -0,0 +1,40 @@
+import {
+  GET_MUNICIPIO_FAILURE,
+  GET_MUNICIPIO_STARTED,
+  GET_MUNICIPIO_SUCCESS,
+} from '../actionTypes/actionTypes';
+
+const getCidades = (id) => {
+  return (dispatch) => {
+    dispatch(getCidadesStarted());
+
+    fetch(
+      `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/municipios`,
+    )
+      .then((res) => res.json())
+      .then((json) => dispatch(getCidadesSuccess(json)))
+      .catch((error) => dispatch(getCidadesFailure(error)));
+  };
+};
+
+const getCidadesStarted = () => {
+  return {
+    type: GET_MUNICIPIO_STARTED,
+  };
+};
+
+const getCidadesSuccess = (data) => {
+  return {
+    type: GET_MUNICIPIO_SUCCESS,
+    payload: data,
+  };
+};
+
+const getCidadesFailure = ({ error }) => {
+  return {
+    types: GET_MUNICIPIO_FAILURE,
+    payload: error,
+  };
+};
+
+export default getCidades;
