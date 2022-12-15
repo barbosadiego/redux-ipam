b8cddc1b37ad1be707fe53235c4b2ae424b1e2ca
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-15 15:48:40 -0300

    add methods and structure for cidades display comp

--- src/components/CidadesDisplay.jsx
+++ src/components/CidadesDisplay.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import getCidades from '../actionCreators/cidadesActions';
 
 const CidadesDisplay = ({ id }) => {
+  const [cidadeId, setCidadeId] = useState(null);
   const store = useSelector((store) => store);
-  console.log(store.cidadesReducer);
+  const dispatch = useDispatch();
+  const cidadesArray = store.cidadesReducer.cidadesData;
+  const inputRef = useRef();
 
-  return <div>CidadesDisplay{id}</div>;
+  useEffect(() => {
+    dispatch(getCidades(id));
+  }, [id]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const inputSelection = inputRef.current;
+    const id = inputSelection.options[inputSelection.selectedIndex].value;
+    setCidadeId(id);
+  }
+
+  console.log(cidadeId);
+  return (
+    <div>
+      <h2>Selecione um município</h2>
+      <form>
+        <label htmlFor="municipios">
+          <select ref={inputRef} name="municipios" id="municipios">
+            {cidadesArray.map((item) => (
+              <option key={item.id} value={item.id}>
+                {item.nome}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button type="submit" onClick={handleSubmit}>
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CidadesDisplay;
