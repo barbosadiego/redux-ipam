981ba83d32d4c9956f4a2a73edc113cb51882051
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-15 15:25:13 -0300

    add cidades reducer to combine reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import estadoReducer from './estadoReducer';
+import cidadesReducer from './cidadesReducer';
 
-export default combineReducers({ estadoReducer });
+export default combineReducers({ estadoReducer, cidadesReducer });
