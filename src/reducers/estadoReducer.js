76d3749aa108899dc6f477f27da06357140908c9
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-15 11:47:20 -0300

    create estado reducer

---
+++ src/reducers/estadoReducer.js
@@ -0,0 +1,43 @@
+import {
+  GET_ESTADO_FAILURE,
+  GET_ESTADO_STARTED,
+  GET_ESTADO_SUCCESS,
+} from '../actionTypes/actionTypes';
+
+const initialState = {
+  loading: false,
+  estadosData: [],
+  error: null,
+};
+
+const estadoReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_ESTADO_STARTED:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case GET_ESTADO_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        estadosData: payload,
+      };
+
+    case GET_ESTADO_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: payload.error,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default estadoReducer;
