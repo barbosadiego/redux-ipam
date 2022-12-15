ab2fdd683b1ef6588ad3886b48ef6c62c024644d
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-15 16:26:58 -0300

    initial info display structure

--- src/components/InfoDisplay.jsx
+++ src/components/InfoDisplay.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const InfoDisplay = ({ data }) => {
-  console.log(data);
+  // console.log(data);
 
-  return <div>InfoDisplay</div>;
+  if (!data[0]) return null;
+
+  return (
+    <div>
+      <h2>Informações do município</h2>
+      <p>microrregião: {data[0].municipio.microrregiao.nome}</p>
+      <p>mesorregião: {data[0].municipio.microrregiao.mesorregiao.nome}</p>
+      <p>UF: {data[0].municipio.microrregiao.mesorregiao.UF.nome}</p>
+      <p>região do município: {data[0].municipio['regiao-imediata'].nome}</p>
+    </div>
+  );
 };
 
 export default InfoDisplay;
