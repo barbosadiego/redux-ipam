dce049d432c77c6308b3bae222814d18d7ecf230
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-15 15:24:51 -0300

    create cidades reducer

---
+++ src/reducers/cidadesReducer.js
@@ -0,0 +1,43 @@
+import {
+  GET_MUNICIPIO_FAILURE,
+  GET_MUNICIPIO_STARTED,
+  GET_MUNICIPIO_SUCCESS,
+} from '../actionTypes/actionTypes';
+
+const initialState = {
+  loading: false,
+  cidadesData: [],
+  error: null,
+};
+
+const cidadesReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_MUNICIPIO_STARTED:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case GET_MUNICIPIO_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        cidadesData: payload,
+        error: null,
+      };
+
+    case GET_MUNICIPIO_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: payload.error,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default cidadesReducer;
