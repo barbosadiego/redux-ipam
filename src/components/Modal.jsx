5ab9481d213ce3da358fa0d20b883e37aa9ea3a7
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-16 15:12:19 -0300

    add Modal config

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import municipio from '../assets/municipios.json';
 import Map from './Map';
 
-const Modal = ({ data }) => {
-  console.log(municipio[0]);
+const Modal = ({ location, openModal }) => {
+  const modalRef = useRef();
+  const { latitude, longitude } = municipio.find(
+    (item) => item.codigo_ibge === location,
+  );
+
   return (
-    <StyledModal>
-      <Map />
+    <StyledModal ref={modalRef}>
+      <button onClick={() => openModal(false)}>Fechar</button>
+      <Map lat={latitude} long={longitude} />
     </StyledModal>
   );
 };
@@ -15,10 +20,25 @@ const Modal = ({ data }) => {
 export default Modal;
 
 const StyledModal = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   position: absolute;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+
+  & button {
+    background-color: red;
+    @media (max-width: 730px) {
+      position: absolute;
+      z-index: 1000;
+      border-radius: 0;
+      top: 0px;
+      right: 0px;
+    }
+  }
 `;
