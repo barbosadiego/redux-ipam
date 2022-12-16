7a02482cdadfa4dbb8e65fbb3ce014b2bf03a36c
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-16 09:47:22 -0300

    add strong tag in InfoDisplay.jsx

--- src/components/InfoDisplay.jsx
+++ src/components/InfoDisplay.jsx
@@ -8,10 +8,21 @@ const InfoDisplay = ({ data }) => {
   return (
     <div>
       <h2>Informações do município</h2>
-      <p>microrregião: {data[0].municipio.microrregiao.nome}</p>
-      <p>mesorregião: {data[0].municipio.microrregiao.mesorregiao.nome}</p>
-      <p>UF: {data[0].municipio.microrregiao.mesorregiao.UF.nome}</p>
-      <p>região do município: {data[0].municipio['regiao-imediata'].nome}</p>
+      <p>
+        Microrregião: <strong>{data[0].municipio.microrregiao.nome}</strong>
+      </p>
+      <p>
+        Mesorregião:{' '}
+        <strong>{data[0].municipio.microrregiao.mesorregiao.nome}</strong>
+      </p>
+      <p>
+        UF:{' '}
+        <strong>{data[0].municipio.microrregiao.mesorregiao.UF.nome}</strong>
+      </p>
+      <p>
+        Região do Município:{' '}
+        <strong>{data[0].municipio['regiao-imediata'].nome}</strong>
+      </p>
     </div>
   );
 };
