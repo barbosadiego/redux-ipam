2163d96e76a67800df740180d0b7470d7914a189
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-17 10:27:03 -0300

    add coments in Map.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
+// este componente recebe os valores de
+// latitude e longitude para renderizar
+// o mapa na tela
 const Map = ({ lat, long }) => {
   return (
     <MapContainer center={[lat, long]} zoom={13} scrollWheelZoom={true}>
